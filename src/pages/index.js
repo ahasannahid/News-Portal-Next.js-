6a4a85e2791f39c2f469736a5b6d4f73fe77c0c2
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-21 09:39:59 +0600

    getStaticProps function for data fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
 
 
-const HomePage = () => {
+const HomePage = ({allNews}) => {
   return (
     <>
       <Head>
@@ -23,4 +23,31 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+/**
+ * getStaticProps er structure(export korte hobe)
+ 
+ export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  return {
+    props:{
+    }
+  };
+};
+
+ * 
+ */
+
+// data fetching
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  // console.log(data);  //client side e run korena. tai browser console e kono data show korbe na. data show korbe jei terminal theke project run hocche sei terminal e
+  return {
+    props:{
+      allNews:data,
+    }
+  };
 };
